f670b120d4234627d80027bdb9d8ad7450fc8407
Najish anjum <najishanjum058@gmail.com>
2026-01-03 21:45:15 +0530

    main3

--- Sol.1.py
+++ Sol.1.py
@@ -1,4 +1,4 @@
 # Q3.py - Program to Add Two Integers
-a = int (input("Enter a desired number:"))
+c = int (input("Enter a desired number:"))
 b = int (input("Enter another desired number:"))
-print (f"The sum of {a} and {b} is {a + b}")
+print (f"The sum of {c} and {b} is {c + b}")
